ada037c72807f09dd0de91dadd2e4f46b782cc61
Kentin-dev <quentin.bregeon23@gmail.com>
2020-02-21 00:50:32 +0100

    uip

--- src/views/registration/Registration.js
+++ src/views/registration/Registration.js
@@ -25,15 +25,29 @@ class Registration extends React.Component {
     return (
       <form onSubmit={this.handleFormSubmit}>
 				<small>* : champs requis</small>
-    
-				<TextField id="pseudo" name="pseudo" label="Votre pseudo" required value={this.state.pseudo} onChange={e => this.setState({pseudo: e.target.value})} />
-				<TextField type="email" id="email" name="email" label="Votre email" required value={this.state.email} onChange={e => this.setState({email: e.target.value})}/>
-				<TextField id="password" name="password" label="Mot de passe" required value={this.state.password} onChange={e => this.setState({password: e.target.value})} />
-				<TextField id="c_password" name="c_password" label="Confirmation" required value={this.state.c_password} onChange={e => this.setState({c_password: e.target.value})}/>
+				<TextField id="pseudo" name="pseudo" label="Votre pseudo" required 
+					value={this.state.pseudo} 
+					onChange={e => this.setState({pseudo: e.target.value})}
+				/>
+
+				<TextField type="email" id="email" name="email" label="Votre email" required 
+					value={this.state.email} 
+					onChange={e => this.setState({email: e.target.value})}
+				/>
+
+				<TextField id="password" name="password" label="Mot de passe" required 
+					value={this.state.password} 
+					onChange={e => this.setState({password: e.target.value})}
+				/>
+
+				<TextField id="c_password" name="c_password" label="Confirmation" required 
+					value={this.state.c_password} 
+					onChange={e => this.setState({c_password: e.target.value})}
+				/>
+
 				<Button variant="contained" color="primary" type="submit" value="Submit" onClick={e => this.handleFormSubmit(e)}>S'inscrire</Button>
       </form>
     );
   }
 }
-
 export default Registration;
